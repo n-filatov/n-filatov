53f43fbe46e1cb7bd5314194fa5cbc929c561a63
Nick Filatov <keddofilatov@gmail.com>
2018-12-02 17:36:23 +0600

    Initail post in my blog

---
+++ src/components/Bio.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+// Import typefaces
+import 'typeface-montserrat'
+import 'typeface-merriweather'
+
+import profilePic from './profile-pic.jpg'
+import { rhythm } from '../utils/typography'
+
+class Bio extends React.Component {
+  render() {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          marginBottom: rhythm(2.5),
+        }}
+      >
+        <img
+          src={profilePic}
+          alt={`Kyle Mathews`}
+          style={{
+            marginRight: rhythm(1 / 2),
+            marginBottom: 0,
+            width: rhythm(2),
+            height: rhythm(2),
+            borderRadius: '100%'
+          }}
+        />
+        <p>
+          Personal blog by{' '}
+          <a href="https://twitter.com/n__filatov">Nikita Filatov</a>
+          <br/>I`m writing about programming and life
+        </p>
+      </div>
+    )
+  }
+}
+
+export default Bio
---
+++ src/pages/first-post/index.md
@@ -0,0 +1,14 @@
+---
+title: First Post
+date: "2018-12-02T22:12:03.284Z"
+---
+
+Hi folks! My name is Nikita. I`m a front-end developer. This is the first post on my blog.
+
+##What goal of this blog?
+
+- First and main goal: I want to improve my skills in writing on English.
+- Second, sometimes a want to share my thoughts about something, and I want to share them here.
+
+##What kind of content will be?
+There may be different content. Technical articles about front end and not only, books review that I have read, articles about my life
